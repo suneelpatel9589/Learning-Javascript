4380114b75131eca24544aee94829a082f4ef982
Suneelpatel9589 <suneelpatel9589@gmail.com>
2025-08-20 20:00:37 +0530

    all file

---
+++ 9day.js
@@ -0,0 +1,21 @@
+/*
+let num = parseInt(prompt("enter a number"))
+if(num%2==0){
+    for(a=10;a>=1; a--)
+    {
+        console.log(num*a)
+    }
+}
+else{
+    console.log(num)
+}
+    */
+
+for(let a = 1; a<=15; a++){
+    if(a==5 || a==10){
+        console.log("")
+    }
+    else{
+        console.log(a);
+    }
+}
---
+++ 9daynes.js
@@ -0,0 +1,10 @@
+
+for(r = 1; r<=5; r++){
+    for(c = 1; c<=r;c++){
+        document.write("*")
+
+    }
+        
+    document.write("<br>")
+
+}
