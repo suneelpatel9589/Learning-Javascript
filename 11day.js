a995fe5152059f1446ec0a887eee4068ee1fc4ae
Suneelpatel9589 <suneelpatel9589@gmail.com>
2025-08-22 19:51:06 +0530

    fun

---
+++ 11day.js
@@ -0,0 +1,34 @@
+/*
+//with argument and no return
+
+function Add(num)
+{
+
+    console.log(num+num)
+}
+Add(10)
+
+//wap to find aera of circle using type ?
+
+function aera(a)
+{
+
+    console.log(3.14*r*r)
+}
+let r = parseInt(prompt("enter a radius"))
+aera(r)
+*/
+//wap to check no is even or odd using user input
+
+function Add()
+{
+if(num%2 == 0){
+    console.log("even",num)
+}
+else{
+    console.log("odd" ,num)
+}
+    console.log()
+}
+let num = parseInt(prompt("enter a number"))
+Add(num)
