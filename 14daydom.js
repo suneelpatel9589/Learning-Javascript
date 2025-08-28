92b50eb35ba8ab6c68a2e7f14a226719c7267bd2
Suneelpatel9589 <suneelpatel9589@gmail.com>
2025-08-28 23:14:25 +0530

    dom

--- 14daydom.js
+++ 14daydom.js
@@ -2,16 +2,32 @@
 //elements--node
 //text--textnode
 
-document.write("<h1>hello<br>world !!</h1>")
+//document.write("<h1>hello<br>world !!</h1>")
 
-let hd = document.getElementById("head")
-//console.log("hd")
+
+//let hd = document.getElementById("head")
+//console.log(hd)
 //hd.textContent = " mr. suneel patel"
-let h = hd.textContent
-console.log(h);
-hd.innerHTML = "<u>again</u>"
+//let h = hd.textContent
+//console.log(h);
+//hd.innerHTML = "<u>again</u>"
 
-let head2 = document.getElementById("head2")
-head2.setAttribute("align","center")
+///let head2 = document.getElementById("head2")
+//head2.setAttribute("align","center")
 
-document.body.removeAttribute('bgcolor')
+//document.body.removeAttribute('bgcolor')
+
+let sir = document.getElementById("sir")
+
+sir.style.textAlign = "center"
+//sir.textContent = "friend"
+console.log(sir)
+//sir.setAttribute("align","center")
+sir.innerHTML = "jhgfd"
+
+
+
+
+
+
+
---
+++ 15day.js
@@ -0,0 +1,20 @@
+ let  head = document.getElementById("hero")
+ head.textContent= "change my jss file"
+
+
+ let hero1 = document.getElementById("hero1")
+ hero1.innerHTML =" <i>cybrom</i>"
+ 
+ let list = document.getElementById("list")
+ list.removeAttribute("type")
+ list.getAttribute("type","squre")
+
+ let img = document.getElementById("img")
+ img.src = "untitled design.png"
+
+ document.getElementById("name").style.backgroundColor = "skyblue"
+
+ let stt =document.getElementById("name")
+ stt.style.padding = "20px"
+ stt.style.color = "green"
+ stt.style.textAlign = "center"
