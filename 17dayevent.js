f23267d57d66c5cc3008e731daec9e21511ecaf5
Suneelpatel9589 <suneelpatel9589@gmail.com>
2025-09-03 13:22:43 +0530

    event

--- 17dayevent.js
+++ 17dayevent.js
@@ -4,13 +4,31 @@ function fun(){
     alert("working...")
 }
 
-
+//text content change on click
 function fun1(){
 let h = document.getElementById("hero")
  h.textContent = "java script"
  
 }
-function red(){
+//color change on click
+
+
+
+function blue(){
 document.getElementById('cname').textContent = "red"
     document.body.style.backgroundColor = "red"
 }
+function blue(){
+document.getElementById('cname').textContent = "blue"
+    document.body.style.backgroundColor = "blue"
+}
+
+function blue(){
+document.getElementById('cname').textContent = "yellow"
+    document.body.style.backgroundColor = "yellow"
+}
+
+
+
+
+
--- home.js
+++ home.js
@@ -1,34 +1,46 @@
-/*
-let p = document.getElementById('p')
-p.style.cssText = "text-align:center; color:red;"
-
-let c = document.createElement('img')
-c.src = "pic.png"
-
-document.body.append(c)
-
-let ul = document.createElement('ul')
-let li = document.createElement('li')
-li.textContent = 'home'
-let li1 = document.createElement('li')
-li1.textContent = 'contact'
-let li2 = document.createElement('li')
-li2.textContent = 'security'
-ul.append(li,li1,li2)
-li.style = "type","square"
-document.body.append(ul)
 
 
-let sec = document.createElement('section')
-let div = document.createElement('div')
-let h = document.createElement('h1')
-h.textContent = 'my name is suneel patel i am a study'
-div.append(h)
-sec.append(div)
-sec.style.cssText = 'background-color:red; padding:40px;color:skyblue;text-align:center'
-document.body.append(sec)
-console.log(sec)
+function sum() {
 
+        let btn = document.getElementById('btn');
+        let current = document.body.style.backgroundColor;
 
-*/
+        if (current === "red") {
+            document.body.style.backgroundColor = "blue";
+            btn.textContent = "blue";
+        } 
+        else if (current === "blue") {
+            document.body.style.backgroundColor = "yellow";
+           btn.textContent = "yellow";
+        } 
+        else {
+            document.body.style.backgroundColor = "red";
+        btn.textContent = "red";
+        }
+       
+    }
 
+    function fun(){
+    
+        document.body.style.backgroundColor = 'red'
+alert('working process...')
+    }
+
+    let sec = document.createElement('section')
+    let div = document.createElement('div')
+    let h = document.createElement('h1')
+    h.textContent = 'my name is suneel patel '
+    div.append(h)
+    sec.append(div)
+    sec.style.backgroundColor = 'red'
+    document.body.append(sec)
+    console.log(sec)
+
+    let btn = document.createElement('button' )
+    btn.textContent = 'enter'
+    btn.onmouseleave= function(){
+        btn.style.backgroundColor = 'yellow'
+        alert('working...');
+    }
+    document.body.append(btn)
+    console.log(btn)
