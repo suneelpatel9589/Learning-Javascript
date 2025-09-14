f1205406e054b85560c69dd0dd12fc042df0b763
Suneelpatel9589 <suneelpatel9589@gmail.com>
2025-09-14 23:42:12 +0530

    o

--- 23day-callback.js
+++ 23day-callback.js
@@ -1,16 +1,22 @@
 
 //function demo(function){
-    //callback function
+    //callback function एक function जिसे आप दूसरे function के अंदर argument (parameter) के रूप में पास करते हो, 
+    // और फिर वो function बाद में call किया जाता है, उसे callback function कहते हैं।
 //}
 
 //higher order
 //settime out two argu pass hoge  timer mini second me likhege
 
 
+
 setTimeout(() => {
-    alert(img)},5000)
+    alert("img")},5000)
 
 setTimeout(() => {
     let img = document.getElementById('img')
+    img.src = 'watch.webp'
     img.style.display ='block'},3000)
     
+   setTimeout(() => {
+    alert('suneel')
+   },2000)
